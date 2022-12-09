d40f9db04c95591e98dc9599a5c410ddb530c367
PoloBongo <104658619+PoloBongo@users.noreply.github.com>
2022-12-09 10:57:29 +0100

    Add files via upload

--- Home.js
+++ Home.js
@@ -54,9 +54,9 @@ function Home() {
       <h1 className="position-category-action">Catégorie : Action</h1>
       <div className="row-img">
         {/* map pour récupérer les films de la page d'api */}
-      {filterListMoovieCategoryAction.map((moovie_home, index) => {
+      {filterListMoovieCategoryAction.map((moovie_home) => {
         return (
-          <div keyz={moovie_home}>
+          <div keya={moovie_home}>
               <div className="size-img-category-action">
             <div className="card">
               <a><img alt="" className="image" src={`https://image.tmdb.org/t/p/original/${moovie_home.poster_path}`}></img></a>
@@ -76,25 +76,25 @@ function Home() {
       })}</div>
     
 <div className="category-aventure">
-<h1 className="position-category-action">Catégorie : Aventure</h1>
+  <h1 className="position-category-action">Catégorie : Aventure</h1>
     <div className="row-img">
-      {filterListMoovieCategoryAventure.map((moovie_category_aventure, index) => {
+      {filterListMoovieCategoryAventure.map((moovie_category_aventure) => {
         if (moovie_category_aventure.genre_ids[1] === 12) {
             return (
-              <div keyw={moovie_category_aventure}>
-              <div className="size-img-category-aventure">
-                <div className="card">
-                  <a><img alt="" className="image" src={`https://image.tmdb.org/t/p/original/${moovie_category_aventure.poster_path}`}></img></a>
-                  <div className="content">
-                  <i className="fa-solid"><pre className="size-date" style={{color:"yellow"}}>Date de Sortie : {moovie_category_aventure.release_date } </pre> </i>
-                  <i style={{position: "absolute", left:"70px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"95px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"120px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"145px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"170px", top:"34px"}} className="fa-regular fa-star"></i>
-                  <span style={{position: "absolute", left:"195px", top:"32px"}}> 4/5</span>
-                </div>
-                </div>
+              <div keyb={moovie_category_aventure}>
+                <div className="size-img-category-aventure">
+                  <div className="card">
+                    <a><img alt="" className="image" src={`https://image.tmdb.org/t/p/original/${moovie_category_aventure.poster_path}`}></img></a>
+                    <div className="content">
+                      <i className="fa-solid"><pre className="size-date" style={{color:"yellow"}}>Date de Sortie : {moovie_category_aventure.release_date } </pre> </i>
+                      <i style={{position: "absolute", left:"70px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"95px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"120px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"145px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"170px", top:"34px"}} className="fa-regular fa-star"></i>
+                      <span style={{position: "absolute", left:"195px", top:"32px"}}> 4/5</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             )
@@ -112,39 +112,39 @@ function Home() {
                   <i style={{position: "absolute", left:"170px", top:"32px"}} className="fa-regular fa-star"></i>
                   <span style={{position: "absolute", left:"195px", top:"32px"}}> 3/5</span>
                 </div>
-                </div>
-                </div>
+              </div>
+            </div>
           )
       }
       })}</div>
 </div>
 <div className="category-animation">
-<h1 className="position-category-action">Catégorie : Animation</h1>
+  <h1 className="position-category-action">Catégorie : Animation</h1>
     <div className="row-img2">
-      {filterListMoovieCategoryAnimation.map((moovie_category_animation, index) => {
+      {filterListMoovieCategoryAnimation.map((moovie_category_animation) => {
         if (moovie_category_animation.genre_ids[0] === 16 || moovie_category_animation.genre_ids[2] === 16) {
             return (
-              <div keys={moovie_category_animation}>
-            <div className="size-img-category-animation">
-              <div className="card">
-                  <a><img alt="" className="image" src={`https://image.tmdb.org/t/p/original/${moovie_category_animation.poster_path}`}></img></a>
-                  <div className="content">
-                  <i className="fa-solid"><pre className="size-date2" style={{color:"yellow"}}>Date de Sortie : {moovie_category_animation.release_date } </pre> </i>
-                  <i style={{position: "absolute", left:"0px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"25px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"50px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"75px", top:"34px"}} className="fa-solid fa-star"></i>
-                  <i style={{position: "absolute", left:"100px", top:"34px"}} className="fa-regular fa-star"></i>
-                  <span style={{position: "absolute", left:"125px", top:"32px"}}> 4/5</span>
-                </div>
-                </div>
+              <div keyc={moovie_category_animation}>
+                <div className="size-img-category-animation">
+                  <div className="card">
+                    <a><img alt="" className="image" src={`https://image.tmdb.org/t/p/original/${moovie_category_animation.poster_path}`}></img></a>
+                    <div className="content">
+                      <i className="fa-solid"><pre className="size-date2" style={{color:"yellow"}}>Date de Sortie : {moovie_category_animation.release_date } </pre> </i>
+                      <i style={{position: "absolute", left:"0px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"25px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"50px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"75px", top:"34px"}} className="fa-solid fa-star"></i>
+                      <i style={{position: "absolute", left:"100px", top:"34px"}} className="fa-regular fa-star"></i>
+                      <span style={{position: "absolute", left:"125px", top:"32px"}}> 4/5</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             )
       }
       })}</div>
 </div>
-    </div>
+</div>
   );
 }
 
